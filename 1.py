44468a7869c8464256d7aff29a25ac50c1bc73e2
c07deborah-cmte <c07deborah@gmail.com>
2026-01-12 23:45:04 -0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,19 @@
+print("ola mundo")
+
+nome = 'deborah'
+idade = 22
+altura = 1.65
+print(nome, idade, altura)
+
+nome = input('qual o seu nome? ')
+idade = input('quantos anos voce tem? ')
+altura = input('qual a sua altura? ')
+print(nome, idade, altura)
+
+nome = input('qual o seu nome? ')
+print('Seja bem vinda, {}!' .format(nome))
+
+dia = input('qual dia voce nasceu? ')
+mes = input('qual mes voce nasceu? ')
+ano = input('qual ano voce nasceu? ')
+print('voce nasceu no dia {} do mes {} do ano {}, certo?'.format(dia, mes, ano))
